788894f74207b729a92ee08c7ccd0e2d0a9de3d0
gucla <guclafreelancer@gmail.com>
2018-10-15 23:17:50 +0100

    15102018

---
+++ server.js
@@ -0,0 +1,8 @@
+require('rtcmulticonnection-server')();
+
+/*
+require('rtcmulticonnection-server')({
+    config: __dirname + resolveURL('/config.json'),
+    logs: __dirname + resolveURL('/logs.json')
+});
+*/
